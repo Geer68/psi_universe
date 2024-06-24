ce5d70d704f7a65c5cc55963539e935be8798481
Gabsplat <gaabgames@gmail.com>
2024-06-24 19:58:22 -0300

    Agregadas cositas de AWS

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,4 +1,4 @@
-import { listEvents } from "@/utils/calendar";
+import { EVENTS_STATE_ENUM, listEvents } from "@/utils/calendar";
 import { NextApiRequest, NextApiResponse } from "next";
 
 interface Request {
@@ -6,9 +6,52 @@ interface Request {
   title: string;
 }
 
+// {
+//   kind: 'calendar#event',
+//   etag: '"3438538322704000"',
+//   id: '4c5qm4b9v158qkrvhu61t9vp1m_20240912T153000Z',
+//   status: 'confirmed',
+//   htmlLink: 'https://www.google.com/calendar/event?eid=NGM1cW00Yjl2MTU4cWtydmh1NjF0OXZwMW1fMjAyNDA5MTJUMTUzMDAwWiA5MzAxNzE3OWE5ZmNkYzBmYTZjOTNiZTI5YzZjNjNlNGVmMDBhNTkxYjk1MzVkMGZjMDk2MjlkZTAwMTk0YjljQGc',
+//   created: '2024-05-15T18:54:38.000Z',
+//   updated: '2024-06-24T22:46:01.352Z',
+//   summary: 'Turno-JUEVES12:30',
+//   creator: { email: 'gaabgames@gmail.com' },
+//   organizer: {
+//     email: '93017179a9fcdc0fa6c93be29c6c63e4ef00a591b9535d0fc09629de00194b9c@group.calendar.google.com',
+//     displayName: 'Turnos',
+//     self: true
+//   },
+//   start: {
+//     dateTime: '2024-09-12T12:30:00-03:00',
+//     timeZone: 'America/Argentina/Mendoza'
+//   },
+//   end: {
+//     dateTime: '2024-09-12T13:30:00-03:00',
+//     timeZone: 'America/Argentina/Mendoza'
+//   },
+//   recurringEventId: '4c5qm4b9v158qkrvhu61t9vp1m',
+//   originalStartTime: {
+//     dateTime: '2024-09-12T12:30:00-03:00',
+//     timeZone: 'America/Argentina/Mendoza'
+//   },
+//   iCalUID: '4c5qm4b9v158qkrvhu61t9vp1m@google.com',
+//   sequence: 0,
+//   reminders: { useDefault: true },
+//   eventType: 'default'
+// },
+
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   //   const body = await req.body.json().then((data: Request) => data);
   //   console.log("body", body);
-  console.log(listEvents());
+  const events = await listEvents(
+    "93017179a9fcdc0fa6c93be29c6c63e4ef00a591b9535d0fc09629de00194b9c@group.calendar.google.com"
+  );
+  if (
+    events === EVENTS_STATE_ENUM.error ||
+    events === EVENTS_STATE_ENUM.empty
+  ) {
+    res.send({ success: false });
+  }
+  console.log(events);
   res.send({ success: true });
 }
--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,42 +1,53 @@
 const { authenticate } = require("@google-cloud/local-auth");
-const { google } = require("googleapis");
-
+import { google } from "googleapis";
 // If modifying these scopes, delete token.json.
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
+export enum EVENTS_STATE_ENUM {
+  success,
+  empty,
+  error,
+}
+
 async function getAuth() {
   const client = new google.auth.JWT(
     process.env.CLIENT_EMAIL,
-    null,
+    undefined,
     process.env.CLIENT_SECRET,
     SCOPES
   );
   return client;
 }
 
-export async function listEvents() {
-  const auth = await getAuth();
-  const calendar = google.calendar({ version: "v3", auth });
-  const res = await calendar.events.list({
-    calendarId:
-      "93017179a9fcdc0fa6c93be29c6c63e4ef00a591b9535d0fc09629de00194b9c@group.calendar.google.com",
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: "startTime",
-  });
-  const events = res.data.items;
-  if (!events || events.length === 0) {
-    console.log("No upcoming events found.");
-    return;
+const id =
+  "93017179a9fcdc0fa6c93be29c6c63e4ef00a591b9535d0fc09629de00194b9c@group.calendar.google.com";
+
+export async function listEvents(
+  calendarId: string
+): Promise<EVENTS_STATE_ENUM | Array<Object>> {
+  try {
+    const auth = await getAuth();
+    const calendar = google.calendar({ version: "v3", auth });
+    console.log("Calendar", calendar);
+    const res = await calendar.events.list({
+      calendarId,
+      timeMin: new Date().toISOString(),
+      maxResults: 40,
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+    const events = res.data.items;
+    if (!events || events.length === 0) {
+      console.log("No upcoming events found.");
+      return EVENTS_STATE_ENUM.empty;
+    }
+    return events;
+  } catch (e) {
+    console.error("Error: ", e);
+    return EVENTS_STATE_ENUM.error;
   }
-  console.log("Upcoming 10 events:");
-  events.map((event: any, i: number) => {
-    const start = event.start.dateTime || event.start.date;
-    console.log(`${start} - ${event.summary}`);
-  });
+  //   events.map((event: any, i: number) => {
+  //     const start = event.start.dateTime || event.start.date;
+  //     console.log(`${start} - ${event.summary}`);
+  //   });
 }
