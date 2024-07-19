7a8a8f93cede9823d5b15ba20879ab2176b7cb93
Gabriel Pérez dIEZ <grperezdiez@gmail.com>
2024-07-18 21:15:15 -0300

    test

--- src/pages/api/psicologos.ts
+++ src/pages/api/psicologos.ts
@@ -8,9 +8,5 @@ interface Request {
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const psicologos = await listPsicologos();
-  // const events = await getEvents(psicologo.idCalendario);
-
-  // res.send({ success: true, events, event });
-  // res.send({ psicologo, events, success: true });
   res.send({ psicologos, success: true });
 }
