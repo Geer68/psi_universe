a28a427f7df8439bf0a079b0e6180aeeb0e4a076
Geer68 <germhidalgo@gmail.com>
2024-08-08 19:32:24 -0300

    session delete console

--- src/pages/api/sesion.ts
+++ src/pages/api/sesion.ts
@@ -4,14 +4,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { query, evento } = req.body;
-  console.log("query:", query);
-  console.log("evento:", evento);
   try {
     const payment = await fetchData(query, evento);
-    console.log("payment", payment);
+
     res.send({ payment, success: true });
   } catch (error: any) {
-    console.log("ERROR:", error);
     res
       .status(500)
       .send({ error: "Error en el pago de sesión", success: false });
