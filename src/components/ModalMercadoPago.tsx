d7dda8b1640b31edc863aeeb1c0f435a5a2c7f00
Geer68 <germhidalgo@gmail.com>
2024-09-03 12:13:55 -0300

    warning redireccion

--- src/components/ModalMercadoPago.tsx
+++ src/components/ModalMercadoPago.tsx
@@ -65,14 +65,14 @@ export default function ModalMercadoPago({
           <AlertDialogDescription>
             <div className="pb-4">
               <p>
-                <span className="text-gray-500">Psicólogo:</span>{" "}
+                <span className="text-gray-600">Psicólogo:</span>{" "}
                 {psicologo.apellido}, {psicologo.nombre}
               </p>
               <p>
-                <span className="text-gray-500">Día:</span> {inicio.date}
+                <span className="text-gray-600">Día:</span> {inicio.date}
               </p>
               <p>
-                <span className="text-gray-500">Hora:</span> {inicio.time} -{" "}
+                <span className="text-gray-600">Hora:</span> {inicio.time} -{" "}
                 {fin.time}
               </p>
             </div>
@@ -101,6 +101,27 @@ export default function ModalMercadoPago({
               <span>Email</span>
               <Input name="email" type="email" required />
             </Label>
+            <div
+              className="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+              role="alert"
+            >
+              <svg
+                className="flex-shrink-0 inline w-4 h-4 me-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                <span className="font-medium">¡Importante!</span> Serás
+                redirigido a Mercado Pago. Esperá a volver a la página para
+                confirmar tu sesión.
+              </div>
+            </div>
+
             <Button
               type="submit"
               className="bg-custom-violetaFondoBTN hover:bg-[#391b67]"
